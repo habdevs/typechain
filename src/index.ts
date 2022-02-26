39948a580b7470991220c4b4b29f17e469d9213f
Anton <hramickih@yandex.ru>
2022-02-26 05:43:41 +0000

    feat: add class Block

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string,
+class Block {
+  public id: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(id: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number,) {
+      this.id = id;
+      this.hash = hash;
+      this.previousHash = previousHash;
+      this.data = data;
+      this.timestamp = timestamp;
+    }
 };
 
-const person = {
-  name: "Anton",
-  age: 28,
-  gender: 'man',
+const genesisBlock: Block = new Block(0, '202020202002', '', 'Hello', 123456);
 
-};
+let blockchain = [genesisBlock];
 
-const sayHi = (person: Human): string => 
-`Hello, ${person.name}. You ${person.age} year and you ${person.gender}`
-
-console.log(sayHi(person)); 
-
+console.log(blockchain);
