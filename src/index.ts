a7fcfb2f7813945840ad40529a08e8dde0a0a349
Anton <hramickih@yandex.ru>
2022-02-26 06:27:57 +0000

    feat: create block structure check

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,19 @@ class Block {
   public data: string;
   public timestamp: number;
 
-  static calculateBlockHash = (id: number,
-    hash: string,
+  static calculateBlockHash = (
+    id: number,
     previousHash: string,
     data: string,
-    timestamp: number):string => CryptoJS.SHA256(id + previousHash + data + timestamp).toString();
+    timestamp: number): string =>
+    CryptoJS.SHA256(id + previousHash + data + timestamp).toString();
+  
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.id === 'number' &&
+    typeof aBlock.hash === 'string' &&
+    typeof aBlock.previousHash === 'string' &&
+    typeof aBlock.data === 'string' &&
+    typeof aBlock.timestamp === 'number';
 
   constructor(id: number,
     hash: string,
@@ -27,10 +35,47 @@ class Block {
 
 const genesisBlock: Block = new Block(0, '202020202002', '', 'Hello', 123456);
 
-let blockchain = [genesisBlock];
+let blockchain: [Block] = [genesisBlock];
 
 const getBlockchain = (): Block[] => blockchain;
 
 const getLastBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const lastBlock: Block = getLastBlock();
+  const newId: number = lastBlock.id + 1;
+  const newTimestamp: number = getTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newId,
+    lastBlock.hash,
+    data,
+    newTimestamp);
+  
+  const newBlock: Block = new Block(
+    newId,
+    newHash,
+    lastBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock('hello'), createNewBlock('Bye-bye'));
+
+const isBlockValid = (candidateBlock: Block, lastBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (lastBlock.id + 1 !== candidateBlock.id) {
+    return false;
+  } else if (lastBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if () {
+    
+  }
+  return true;
+};
+
+
